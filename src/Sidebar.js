34279fe2b8f6d623d0890fdac98d146c0f740db4
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2020-12-28 18:36:28 +0530

    add update header part

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,30 @@
 //rfce
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Sidebar.css'
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__top">
+                <img src="" alt="" />
+                   <Avatar className="sidebar_avatar"   />
+                   <h2>Janith Rathnayaa</h2>
+                   <h4>janithrathnayaka@gmail.com</h4>
+            </div>
+
+            <div className="sidebar_stats">
+                   <div className="sidebar_stat">
+                      <p>Who views you</p>
+                      <p className="sidebar_statNumber">2,543</p>
+                   </div>
+                   <div className="sidebar_stat">
+                      <p>Views on post</p>
+                      <p className="sidebar_statNumber">2,500</p>
+                   </div>
+            </div>
+
+            <div className="sidebar_bottom">
+                    <p>Recent</p>
 
             </div>
         </div>
